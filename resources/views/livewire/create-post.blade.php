4b1ec8221dc97321a0f54f2e349a7fd8e298b91c
Achmad Alghifari <31951431+misterquinze@users.noreply.github.com>
2021-04-27 11:25:53 +0700

    create post table & livewire post

---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,7 @@
+<div class="p-4">
+    <textarea name="body" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-none" rows="4"></textarea>
+
+    <div class="mt-2 flex justify-end">
+        <button class="px-4 py-2 bg-blue-600 text-white hover:bg-blue-400 rounded-md">Post</button>
+    </div>
+</div>
